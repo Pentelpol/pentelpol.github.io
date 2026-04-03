5c747cb3cba9bab9df3f051a5c12fcc07aeae934
Pentelpol <102719117+Pentelpol@users.noreply.github.com>
2026-04-03 10:50:58 +0800

    refactor code

--- Admin-Tracker/script/admin-tracker.js
+++ Admin-Tracker/script/admin-tracker.js
@@ -1,12 +1,26 @@
 (() => {
   'use strict'
+  /*
+	fetch('script/config.json')
+		.then(response => response.json())
+		.then(data => {
+			const config = data;
+	});
+	import config from 'script/config.json';
+	//const config = require('script/config.json');
+	console.log(config.databaseURL); // 3000
+	console.log(config.authorization); // 3000
+	*/
+	
+	// Your web app's configuration
+	const appConfig = {
+		"databaseURL": "https://payment-f9c12-default-rtdb.firebaseio.com",
+		"authorization" : "vAU2WgoNg1lMZDcIYMfzMjIWlZ723pYpWxVvj9Hb"
+	}
 
-  /*document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })*/
 	var Loans;
 	
-    $(document).ready(function () {
+    $(document).ready(function ($) {
 		/*
 		const params = new Proxy(new URLSearchParams(window.location.search), {
 		  get: (searchParams, prop) => searchParams.get(prop),
@@ -14,9 +28,15 @@
 		console.log("params");
 		console.log(params.id);
 		*/
-		const d = new Date();
-		let text = d.toUTCString();
-		document.getElementById('start_date_id').valueAsDate  = new Date();
+		// Loading Partial Page
+		$('#logo').load('Shared/logo.html');
+		$('#navbar').load('Shared/navbar.html');
+		$('#floatingbutton').load('Shared/floatingbutton.html');
+		
+		var now = new Date();
+		now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+		let text = now.toISOString().slice(0, 16);
+		document.getElementById('start_date_id').value  = text;
 		let uuid = crypto.randomUUID();
 		document.getElementById('loan_id_id').value  = uuid;
 		
@@ -35,32 +55,7 @@
 		getLoanViaFirebaseAPI();
 		
     });
-	
-	function getLoanViaFirebaseAPI(){
-		// Create a request variable and assign a new XMLHttpRequest object to it.
-		var request = new XMLHttpRequest()
-
-		// Open a new connection, using the GET request on the URL endpoint
-		request.open('GET', 'https://payment-f9c12-default-rtdb.firebaseio.com/loan.json?auth=vAU2WgoNg1lMZDcIYMfzMjIWlZ723pYpWxVvj9Hb&orderBy="id"', true)
-
-		request.onload = function() {
-		 // Begin accessing JSON data here
-		  var data = JSON.parse(this.response)
-
-		  if (request.status >= 200 && request.status < 400) {
-			console.log("getLoans data");
-			console.log(data);
-			loadTableData(data);
-			initialiseDataTable();
-		  } else {
-			console.log('error')
-		  }
-		}
-
-		// Send request
-		request.send()
-	}
-	
+		
 	function loadTableData(items) {
 		console.log(items);
 		var arrayObject;
@@ -73,29 +68,13 @@
 		{
 			var item = items[i];
 			html += `
-				<tr>
+				<tr onclick="window.location='../../Tracker/tracker?id=${item.loan_id}';" style="cursor:pointer;">
 					<td>${item.name}</td>
 					<td>${item.reference_number}</td>
 					<td>${item.amount}</td>
 					<td>${item.loan_status}</td>
 				</tr>
 			`;
-			/*
-			let row = table.insertRow();
-			
-			let name = row.insertCell(0);
-			name.innerHTML = item.name;
-			
-			let reference_number = row.insertCell(1);
-			reference_number.innerHTML = item.reference_number;
-			
-			let amount = row.insertCell(2);
-			amount.innerHTML = item.amount;
-			
-			let loan_status = row.insertCell(3);
-			loan_status.innerHTML = item.loan_status;
-			//status.innerHTML = readTermStatus(item.status);
-			*/
 		}
 		document.getElementById('tableBody').innerHTML = html;
 	}
@@ -121,10 +100,37 @@
         });
 	}
 	// FIREBASE API CALLS
+	
+	function getLoanViaFirebaseAPI(){
+		// Create a request variable and assign a new XMLHttpRequest object to it.
+		var request = new XMLHttpRequest()
+		const url = appConfig.databaseURL + '/loan.json?auth=' + appConfig.authorization +'&orderBy="loan_id"';
+
+		// Open a new connection, using the GET request on the URL endpoint
+		request.open('GET', url, true)
+
+		request.onload = function() {
+		 // Begin accessing JSON data here
+		  var data = JSON.parse(this.response)
+
+		  if (request.status >= 200 && request.status < 400) {
+			console.log("getLoans data");
+			console.log(data);
+			loadTableData(data);
+			initialiseDataTable();
+		  } else {
+			console.log('error')
+		  }
+		}
+
+		// Send request
+		request.send()
+	}
+	
 	function createNewLoan(dataObj)
 	{
 		const xhr = new XMLHttpRequest();
-		const url = "https://payment-f9c12-default-rtdb.firebaseio.com/loan.json?auth=vAU2WgoNg1lMZDcIYMfzMjIWlZ723pYpWxVvj9Hb";
+		const url = appConfig.databaseURL + '/loan.json?auth=' + appConfig.authorization;
 
 		// 1. Initialize the request
 		xhr.open("POST", url, true);
